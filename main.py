2f416ad543b0d1ed2105b24f251f24df43abd53f
pufick-rock <35701918+pufick-rock@users.noreply.github.com>
2018-01-23 00:11:50 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-from 
+from configs import *
+from classes import Engine
+# look at my code
+# it's awesome
+# I follow PEP8
+
+
+if __name__ == 'main':
+  engine = Engine(configs.token)
+  engine.config(configs.engine_configs)
+  engine.start()
+  # be happy
+
+
